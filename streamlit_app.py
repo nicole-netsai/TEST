e4f5db81adb7e7245f61bba617829646de1f8ae5
nicole-netsai <nharaundanicole@gmail.com>
2025-04-06 01:15:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 from datetime import datetime
 import time
 import pandas as pd
-from app.models import ParkingModel
+from app-model import ParkingModel
 from app.utils import ParkingUtils
 from app.config import PARKING_LOTS, ADMIN_CREDENTIALS, GOOGLE_MAPS_API_KEY
 
